e6435bfbb44039638fd01e002159885970fcc02e
Tenzin-Lektsok <tenzinlektsok7@gmail.com>
2026-06-22 19:55:03 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace LearnDoublyLinkedList
+{
+    //Doubly linked contain collections of element represented by nodes,thus create class Node
+
+    class DoublyLinkedList
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
